37fb926239ce75a81ed67c47c642ea8e9333e168
Junzhe Piao <junzhepiao@gmail.com>
2019-01-07 16:43:07 -0700

    basic strucure of index page

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,8 @@ const knex = require("../db/knex.js");
 
 module.exports = {
     index: function(req, res) {
-        knex('users').then((result)=>{
-          res.render("index", {users: result});
+        knex('items').then((result)=>{
+          res.render("index", {items: result});
         })
       }
 
